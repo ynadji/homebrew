5ee6952acac4f313951d9f2786bd1ff7011ffc65
Yacin Nadji <yacin@gatech.edu>
2010-04-30 15:43:50 -0400

    updated highlight formula per adamv's suggestions
    see http://github.com/ynadji/homebrew/commit/557a7441a00ba157102f52063b76f529e1e9c603 for more details.

--- Library/Formula/highlight.rb
+++ Library/Formula/highlight.rb
@@ -5,16 +5,13 @@ class Highlight <Formula
   homepage 'http://www.andre-simon.de/zip/download.html'
   md5 '08f1429a6db258ab1a7eecbb4e0d44b2'
 
-  def fix_makefile
+  def install
+    # fix the makefile
     inreplace 'makefile' do |contents|
       contents.change_make_var! "PREFIX", prefix
-      contents.change_make_var! "man_dir", "${PREFIX}/share/man/man1/"
+      contents.change_make_var! "man_dir", man1
       contents.change_make_var! "conf_dir", "${PREFIX}/etc/highlight/"
     end
-  end
-
-  def install
-    fix_makefile()
     system "make cli"
     system "make install"
   end
