339b2c80a78b61fc8cd56e9c59d1d599d68af96d
Yacin Nadji <yacin@gatech.edu>
2010-08-26 15:15:23 -0400

    Modified libdnet formula to include python support
    Added --with-python flag to libdnet compilation to
    also include dnet bindings for Python. Requires
    a Homebrew Python installation.

--- Library/Formula/libdnet.rb
+++ Library/Formula/libdnet.rb
@@ -5,10 +5,20 @@ class Libdnet <Formula
   homepage 'http://code.google.com/p/libdnet/'
   md5 '9253ef6de1b5e28e9c9a62b882e44cc9'
 
+  def options
+    ["--python", "Python bindings"]
+  end
+
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--mandir=#{man}"
+    args = ["--disable-debug", "--disable-dependency-tracking",
+           "--prefix=#{prefix}",
+           "--mandir=#{man}"]
+
+    if ARGV.include? "--python"
+      args << "--with-python"
+    end
+
+    system "./configure", *args
     system "make install"
   end
 end
